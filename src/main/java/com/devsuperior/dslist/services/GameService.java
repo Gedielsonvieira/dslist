be5b0b71efad21a55389a253bacd12c1f0920cf3
Gedielson Vieira <gedielsonvieira@outlook.com>
2023-05-10 21:58:53 -0300

    Created Game Controller, Service, Repository

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<GameMinDTO> findAllGames() {
+        List<Game> listOfGames = gameRepository.findAll();
+        //ENTENDER O STREAM.MAP
+        return listOfGames.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
